143419f4094dd573d79d8997ba85b1549beb105b
Agustín Juvencio <felixagustinramirez@gmail.com>
2026-08-01 12:32:32 -0600

    APP V2.1

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Sube este número cada vez que publiques cambios en index.html
 // para forzar que los usuarios reciban la versión nueva.
-const CACHE_VERSION = 'liga-konami-v4';
+const CACHE_VERSION = 'liga-konami-v6';
 
 const APP_SHELL = [
   './',
