5c0eb74f93ad36b3722291e0f1b827c746ac3bcc
Agustín Juvencio <felixagustinramirez@gmail.com>
2026-08-04 20:23:03 -0600

    APP NUEVO FORMATO

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Sube este número cada vez que publiques cambios en index.html
 // para forzar que los usuarios reciban la versión nueva.
-const CACHE_VERSION = 'liga-konami-v15';
+const CACHE_VERSION = 'liga-konami-v16';
 
 const APP_SHELL = [
   './',
